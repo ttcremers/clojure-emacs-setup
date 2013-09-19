62d32f978a3b684c881f540edc2e6993832cc739
Thomas Cremers <ttcremers@gmail.com>
2013-09-19 12:24:12 +0200

    added marmalade to repo and setup solarized as default theme

--- init.el
+++ init.el
@@ -1,16 +1,21 @@
-(custom-set-variables
+1(custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ansi-color-faces-vector [default bold shadow italic underline bold bold-italic bold])
+ '(ansi-color-names-vector (vector "#c5c8c6" "#cc6666" "#b5bd68" "#f0c674" "#81a2be" "#b294bb" "#8abeb7" "#1d1f21"))
  '(auto-save-default nil)
  '(backup-inhibited t t)
  '(column-number-mode t)
+ '(custom-safe-themes (quote ("fc5fcb6f1f1c1bc01305694c59a1a861b008c534cae8d0e48e4d5e81ad718bc6" "1e7e097ec8cb1f8c3a912d7e1e0331caeed49fef6cff220be63bd2a6ba4cc365" "06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" default)))
  '(delete-selection-mode t)
+ '(fci-rule-color "#282a2e")
+ '(hl-paren-colors (quote ("#d54e53" "#e78c45" "#e7c547" "#b9ca4a" "#70c0b1" "#7aa6da" "#c397d8")))
  '(inhibit-startup-screen t)
  '(initial-scratch-message nil)
- '(tool-bar-mode nil)
- '(scroll-bar-mode nil))
+ '(scroll-bar-mode nil)
+ '(tool-bar-mode nil))
 
 ;; Fix the PATH variable
 (defun set-exec-path-from-shell-PATH ()
@@ -24,6 +29,8 @@
 (package-initialize)
 (add-to-list 'package-archives
   '("melpa" . "http://melpa.milkbox.net/packages/") t)
+(add-to-list 'package-archives 
+  '("marmalade" . "http://marmalade-repo.org/packages/"))
 
 ;; PLUGINS
 
@@ -58,7 +65,7 @@
 ;; Night Rainbow
 ;; (custom-set-variables '(hl-paren-colors (quote ("#cc6666" "#de935f" "#f0c674" "#b5bd68" "#8abeb7" "#81a2be" "#b294bb"))))
 ;; Bright Rainbow
-(custom-set-variables '(hl-paren-colors (quote ("#d54e53" "#e78c45" "#e7c547" "#b9ca4a" "#70c0b1" "#7aa6da" "#c397d8"))))
+
 
 (add-hook 'clojure-mode-hook 'highlight-parentheses-mode)
 
@@ -86,10 +93,12 @@
 (eval-after-load "color-theme"
   '(progn
      (color-theme-initialize)     
-     (color-theme-sanityinc-tomorrow-night)
+     ;; (color-theme-sanityinc-tomorrow-night)
      ;; (color-theme-sanityinc-tomorrow-bright)
      ;; (color-theme-sanityinc-tomorrow-blue)
      ;; (color-theme-sanityinc-tomorrow-eighties)
+     ;; (color-theme-solarized-dark)
+     (color-theme-solarized-light)
      ))
 
 (set-face-attribute 'default nil :height 120)
@@ -118,3 +127,9 @@
 ;; highlight trainling spaces, empty lines and etc
 ;; (setq whitespace-style '(face empty tabs lines-tail trailing))
 ;; (global-whitespace-mode t)
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
